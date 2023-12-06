3ccbf9078a8af471950dc2878be20c4bfebf1ccb
niagarafalls729 <wltn729@gmail.com>
2023-12-06 21:58:36 +0900

    예전꺼 지우고 다시 공부

---
+++ TS/01_primitiveTypes.ts
@@ -0,0 +1,27 @@
+//기본 타입 정리 
+let userName : string = 'kim';
+// userName = 123;
+userName = '123'; 
+let userAge : number = 7; 
+type gender = 'man'|'girl'
+let userGender : gender = 'man' 
+let grayColor :string[] = ['gray','black','white']; 
+let members :{member1 : string, member2: string} = { member1 : 'kim', member2: 'park'}
+
+///// 온갖변수에 다 쓰지말자. 굳이 ? 
+//굳이 안해도 이미 타입이 적용됨, 
+let myName = 'park';
+//myName = 123;
+myName = 'kim' 
+let pop : {
+    name : string ,
+    title : string 
+} = {
+    name : 'kim' ,
+    title : '애국가'
+} 
+type ColorStandard = {
+    ko: string;
+    en: string;
+  }; 
+let color : ColorStandard[] =  [{ko : '빨강' , en : 'red'},{ ko : '파랑', en : 'blue'}]
